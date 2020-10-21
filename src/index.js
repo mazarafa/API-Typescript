ccec73a499e2a12b6155323fd07e0f73d446a5d0
mazarafa <arruda.rafa.ra@gmail.com>
2020-10-21 11:09:15 -0400

    adicionando metodos HTTP e o insomnia

--- src/index.js
+++ src/index.js
@@ -4,9 +4,41 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({
-        message:'Hello Rafinha'
-    });
+    return response.json([
+        //message:'Hello Rafinha'
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.post('/projects',(request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+
+});
+app.put('/projects',(request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+
+});
+
+app.delete('/projects',(request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+
 });
 
 app.listen(3333, () => {
